b55bfc64c8fe2aa85c401eeaa424584fb6093445
Ivo <irbatistela@gmail.com>
2021-07-17 13:13:01 -0300

    :sparkles: serve 1 pixel image on /touch resource

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+app.get('/touch', (req, res) => {
+  const onePxImageReadStream = fs.createReadStream('./assets/1px.png');
+
+  onePxImageReadStream.on('open', function() {
+    res.set('Content-type', 'image/png');
+    onePxImageReadStream.pipe(res);
+  });
+
+  onePxImageReadStream.on('error', function() {
+    res.status(500).end('Something unexpected happened');
+  });
 });
 
 app.listen(port, () => {
